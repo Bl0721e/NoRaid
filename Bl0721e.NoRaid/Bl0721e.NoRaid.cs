1f393629f2308a6eab9261b4c2bb01d0c6530311
Bl0721e <me@blockie.cc>
2025-11-01 19:13:24 +0800

    why

--- Bl0721e.NoRaid/Bl0721e.NoRaid.cs
+++ Bl0721e.NoRaid/Bl0721e.NoRaid.cs
@@ -40,7 +40,7 @@ namespace Bl0721e.NoRaid
         protected override async UniTask OnUnloadAsync()
         {
             // await UniTask.SwitchToMainThread(); uncomment if you have to access Unturned or UnityEngine APIs
-            m_Logger.LogInformation(Stopped"]);
+            m_Logger.LogInformation("Stopped");
         }
     }
 
